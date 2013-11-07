cb63f2617c5fa3626e047fa0e148fe201a4d6ebb
Tim Lehr <lehr@tzi.de>
2013-11-07 17:35:04 +0100

    Initial commit

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
-#include <digiquartz_pressure/Dummy.hpp>
+#include <digiquartz_pressure/Driver.hpp>
 
 int main(int argc, char** argv)
 {
-	dummy_project::DummyClass dummyClass;
-	dummyClass.welcome();
+	digiquartz_pressure::Driver driver;
+	driver.open(argv[1]);
+        digiquartz_pressure::Config config;
+        driver.setIntegrationTime(10);
 
+        driver.startAcquisition();
+
+        double value;
+        while (driver.readMeasurement(value)){
+            std::cout << value << std::endl;
+        }
 	return 0;
 }
