3eeadddc0dc04d98b53e4a05bd51df2690261c19
AlmeidaJoaoPedro <jpedro.amsilva@gmail.com>
2024-10-23 16:06:29 -0300

    fix cmakelist errors

--- include/pressure_pkg/pressure_node.hpp
+++ include/pressure_pkg/pressure_node.hpp
@@ -1,3 +1,17 @@
+// Copyright (c) 2023, SENAI Cimatec
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #pragma once
 
 #include "pressure_pkg/pressure_driver.hpp"
@@ -13,12 +27,12 @@
 
 class PressureNode : public rclcpp::Node
 {
-  public:
-    PressureNode();
+public:
+  PressureNode();
 
-  private:
-    void timer_callback();
-    rclcpp::TimerBase::SharedPtr timer_;
-    rclcpp::Publisher<std_msgs::msg::Float64>::SharedPtr pressure_publisher_;
-    pressure_pkg::PressureDriver pressure_driver_;
+private:
+  void timer_callback();
+  rclcpp::TimerBase::SharedPtr timer_;
+  rclcpp::Publisher<std_msgs::msg::Float64>::SharedPtr pressure_publisher_;
+  pressure_pkg::PressureDriver pressure_driver_;
 };
--- src/pressure_node.cpp
+++ src/pressure_node.cpp
@@ -21,9 +21,9 @@ class MinimalPublisher : public rclcpp::Node
 {
 public:
   MinimalPublisher()
-    : Node("minimal_publisher"), count_(0), pressure_driver(std::make_shared<pressure_pkg::PressureDriver>())
+  : Node("minimal_publisher"), count_(0),
+    pressure_driver(std::make_shared<pressure_pkg::PressureDriver>())
   {
-    // Corrigido para criar o publisher de Float64
     publisher_ = this->create_publisher<std_msgs::msg::Float64>("topic", 10);
     timer_ = this->create_wall_timer(
       500ms, std::bind(&MinimalPublisher::timer_callback, this));
@@ -33,11 +33,7 @@ private:
   void timer_callback()
   {
     auto message = std_msgs::msg::Float64();
-    
-    // Chame getPressure() na instância de pressure_driver
     message.data = pressure_driver->getPressure();
-    
-    // Usar %f para imprimir o valor do tipo double
     RCLCPP_INFO(this->get_logger(), "Publishing: '%f'", message.data);
     publisher_->publish(message);
   }
